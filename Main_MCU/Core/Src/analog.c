f30884940e4690e0505ad8c4d2cecbcd3cb60528
Ivan Liutov <ivan.liutov@msk.konvels.local>
2022-12-30 15:12:34 +0300

    --amend

--- Main_MCU/Core/Src/analog.c
+++ Main_MCU/Core/Src/analog.c
@@ -7,9 +7,11 @@
 #include <analog.h>
 #include <settings.h>
 
-Pt100_Struct pt100_input_buffer[PT100_INPUT_BUFFER_SIZE];
+Pt100_Struct pt100_input_buffer;
+float pt100_buf[PT100_INPUT_BUFFER_SIZE];
 uint8_t pt100_data_ready;
 uint32_t temp;
+uint8_t j;
 
 
 
@@ -35,36 +37,33 @@ void Analog_Read(void)
 
 static void Calc_Pt100()
 {
+	float sum = 0;
 	if(pt100_data_ready)
 	{
-		float pt100_0, pt100_1, pt100_2, pt100_3, pt100_4, vRef;
 		for (int i = 0; i < PT100_INPUT_BUFFER_SIZE; ++i) {
-			pt100_0 += pt100_input_buffer[i].Pt100_0;
-			pt100_1 += pt100_input_buffer[i].Pt100_1;
-			pt100_2 += pt100_input_buffer[i].Pt100_2;
-			pt100_3 += pt100_input_buffer[i].Pt100_3;
-			pt100_4 += pt100_input_buffer[i].Pt100_4;
-			vRef += pt100_input_buffer[i].V_ref;
+			sum+=pt100_buf[i];
 		}
-		vRef = vRef/PT100_INPUT_BUFFER_SIZE;
-		vRef = 3.3f*(float)V_REF_VALUE/vRef;
-		pt100_0 = pt100_0/PT100_INPUT_BUFFER_SIZE * 3.3f/vRef;
-		pt100_1 = pt100_1/PT100_INPUT_BUFFER_SIZE * 3.3f/vRef;
-		pt100_2 = pt100_2/PT100_INPUT_BUFFER_SIZE * 3.3f/vRef;
-		pt100_3 = pt100_3/PT100_INPUT_BUFFER_SIZE * 3.3f/vRef;
-		pt100_4 = pt100_4/PT100_INPUT_BUFFER_SIZE * 3.3f/vRef;
-
-		float vpt100 = pt100_3 - pt100_1;
-		float ref = pt100_1-pt100_0;
-		float v_result = vpt100*100/ref;
-		meas_data.pt100 = (v_result - 100)*2.5974f;
+		meas_data.pt100 = sum/PT100_INPUT_BUFFER_SIZE;
 		pt100_data_ready = 0;
-		temp++;
 	}
 }
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
+	float pt100_0, pt100_1, pt100_2, pt100_3, pt100_4, vRef;
+	vRef = pt100_input_buffer.V_ref;
+	vRef = 3.3f*(float)V_REF_VALUE/vRef;
+	pt100_0 = pt100_input_buffer.Pt100_0 * 3.3f/vRef;
+	pt100_1 = pt100_input_buffer.Pt100_1 * 3.3f/vRef;
+	pt100_2 = pt100_input_buffer.Pt100_2 * 3.3f/vRef;
+	pt100_3 = pt100_input_buffer.Pt100_3 * 3.3f/vRef;
+	pt100_4 = pt100_input_buffer.Pt100_4 * 3.3f/vRef;
+	float vpt100 = pt100_3 - pt100_1;
+	float ref = pt100_1-pt100_0;
+	float v_result = vpt100*100/ref;
+	pt100_buf[j] = (v_result - 100)*2.5974f;
+	j++;
+	if(j>=PT100_INPUT_BUFFER_SIZE)j=0;
 	pt100_data_ready = 1;
 }
 
